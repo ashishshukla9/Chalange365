ef6aa1d1be97378ab9af02260dedc87419f5fce9
Ashish Shukla <shuklaashish655@gmail.com>
2026-09-27 00:28:16 +0530

    feat(flatten): Add new concept with exact comment

--- Day2.js
+++ Day2.js
@@ -1,20 +1,15 @@
-//Flatten Array Day 2 Practice
+ const arr = [1,[2],3,[4,5],6,[7,8]];
 
-const arr = [1,[2,[3]],4,5,[6,7]];
+ function flatten(arr){
+let result = []; // Empty Array
 
-function flatten(arr){
-
-    let result =[];
-
-    for(let item of arr){
-        if(Array.isArray(item)){
-            result = result.concat(flatten(item))
-        }else{
-            result.push(item)
-        }
-
+for(let item of arr){ // yaha pe mene of use kiya h kyunki js me of values deta h agar mujhe position batana hota to me yaha pe in ka use karta tha 
+    if(Array.isArray(item)){ 
+        result= result.concat(flatten(item));
+    }else{
+        result.push(item);
     }
-    return result
 }
-
-console.log(flatten(arr))
+return result;
+ }
+ console.log(flatten(arr));
