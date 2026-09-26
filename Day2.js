3a5a947ceebcdcb0ad1a4ca9ad399efbb2642bbb
Ashish Shukla <shuklaashish655@gmail.com>
2026-09-26 16:21:59 +0530

    feat(flatten): Creating new flatten example

---
+++ Day2.js
@@ -0,0 +1,20 @@
+//Flatten Array Day 2 Practice
+
+const arr = [1,[2,[3]],4,5,[6,7]];
+
+function flatten(arr){
+
+    let result =[];
+
+    for(let item of arr){
+        if(Array.isArray(item)){
+            result = result.concat(flatten(item))
+        }else{
+            result.push(item)
+        }
+
+    }
+    return result
+}
+
+console.log(flatten(arr))
